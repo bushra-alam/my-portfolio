005b30e69fc8b65fbcaae49145bb78b74c470b78
bushra alam <siba742002@gmail.com>
2025-02-15 04:23:04 -0800

    Resolved scroll down issue

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function showSidebar(){
     let sidebar = document.querySelector(".sidebar")
     sidebar.style.display = "flex"
+    document.documentElement.classList.add("no-scroll")
 }
 
 function hideSidebar(){
     let sidebar = document.querySelector(".sidebar")
     sidebar.style.display = "none"
+    document.documentElement.classList.remove("no-scroll")
 }
